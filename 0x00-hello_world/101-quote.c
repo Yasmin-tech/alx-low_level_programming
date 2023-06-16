9876b19746fb02b507592ec0078a877393dceeb7
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-16 11:14:36 -0700

    Write a C program that prints to the standard erro

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Write a C program that prints exactly
+* and that piece of art is useful" - Dora Korpar,
+* 2015-10-19, followed by a new line, to the
+*standard error.
+*
+* Return: Always 1
+*/
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+        fprintf(stderr, "2015-10-19\n");
+	return (1);
+}
