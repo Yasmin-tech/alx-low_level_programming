576108e1d90aef1287d53350570be1c92b4b8978
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-18 08:33:51 -0700

    write the program 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print character a-z
+*
+* Return (0) Success!
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
