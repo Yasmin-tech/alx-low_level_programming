30d4498612a1c1ae1c44ea92a784c8d9801f8503
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-20 20:37:07 -0700

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -  a function that calulate the sum of two numbers
+ * @num1: The first input number
+ * @num2: The second input number
+ *
+ * Return: sum of num1 and num2
+ */
+
+int add(int num1, int num2)
+{
+	int sum = 0;
+
+	sum = num1 + num2;
+	return (sum);
+}
