991128b97f484b1d1eb74cb430844a7bb01ee0a2
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-22 14:41:32 +0000

    edit 2 add 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,18 @@
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * fizz_buzz - print fizz , buzz, or both, or a number
