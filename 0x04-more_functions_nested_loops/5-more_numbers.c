c9b12bac213084f99762201d0e7c272501332be9
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-22 10:17:52 +0000

    add 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - print digits (0 to 14) 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int num, i;
+
+	for (i = 1; i <= 10; i++)
+	{
+
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+			else
+			{
+				_putchar(num + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
