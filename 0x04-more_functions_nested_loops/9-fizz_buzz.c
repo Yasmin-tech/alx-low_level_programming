65d21c6ea0e372ce232574fb495aebe54c87769b
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-22 14:13:28 +0000

    add 9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - print fizz , buzz, or both, or a number
+ *
+ * Return: void
+ */
+
+void fizz_buzz(void)
+{
+	int i, num = 100;
+
+	for (i = 1; i <= num; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
