8b5499fa6a3c94b8d599d3939ddf0a1d968d2b19
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-22 02:53:34 +0000

    add 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check if a number is positivie, nigative or 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_nigative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,6 @@
+## 0x03. C - Debugging
+
+## You"ll learn :
+What is debugging
+What are some methods of debugging manually
+How to read the error messages
