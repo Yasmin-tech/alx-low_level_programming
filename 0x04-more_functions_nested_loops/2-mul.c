5b458a97840dc0381facd3394bffdbe4ff8250d4
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-22 09:30:50 +0000

    add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiples two digits
+ * @a: the first number 
+ * @b: the second number
+ * Return: the result of a * b
+ */
+
+int mul(int a, int b)
+{
+	int mul_result = a * b;
+
+	return (mul_result);
+}
