c5b416426cb01471a75763329315432c4eb24f1d
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-23 03:11:18 +0000

    add 100-print_factor.c function

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * prime_factor - print largest prime factore of 612852475143
+ *
+ * Return: void
+ */
+
+void prime_factor(void)
+{
+	int i;
+	long int maxPrime = 1;
+	int num = 1231952;
+
+	while (num % 2 == 0)
+	{
+		maxPrime = 2;
+		num /= 2;
+	}
+
+	for (i = 3; i <= sqrt(num); i++)
+	{
+		while (num % i == 0)
+		{
+			maxPrime = i;
+			num /= i;
+		}
+	}
+
+	if (num > 2)
+		maxPrime = num;
+
+	    printf("%ld\n", maxPrime);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: 0 alwayes
+ */
+
+int main(void)
+{
+	prime_factor();
+	return (0);
+}
