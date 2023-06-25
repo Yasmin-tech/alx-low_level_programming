c8d93f612361c2260b8582e5553fcabfb816339e
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-25 18:29:03 +0000

    edit by adding %09 padding with Zeros if the number is less than 9 digits, 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,13 +9,14 @@
 
 void print_fib_98(void)
 {
-	unsigned long int i, a = 0, b = 1, result, l = 1000000000;
+	unsigned long int i, a = 1, b = 2, result, l = 1000000000;
 	unsigned long int a1, a2, b1, b2, result1, result2;
 
-	for (i = 0; i < 91; i++)
+	printf("%lu, %lu", a, b);
+	for (i = 0; i < 90; i++)
 	{
 		result = a + b;
-		printf("%lu, ", result);
+		printf(", %lu", result);
 
 		a = b;
 		b = result;
@@ -26,16 +27,13 @@ void print_fib_98(void)
 	b1 = b / l;
 	b2 = b % l;
 
-	for (i = 92; i < 99; i++)
+	for (i = 93; i < 99; i++)
 	{
 		result1 = a1 + b1;
 		result2 = a2 + b2;
 
-		printf("%lu", result1);
+		printf(", %lu", result1);
 		printf("%09lu", result2);
-
-		if (i != 98)
-			printf(", ");
 
 		a1 = b1 + (b2 / l);
 		a2 = b2 % l;
