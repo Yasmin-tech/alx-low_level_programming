72fb17508bb53a60513a7861ffc1b01f14fa308f
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-25 17:55:10 +0000

    edit 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,38 @@
 
 void print_fib_98(void)
 {
-	unsigned long int i, a = 0, b = 1;
-	unsigned long int result;
+	unsigned long int i, a = 0, b = 1, result, l = 1000000000;
+	unsigned long int a1, a2, b1, b2, result1, result2;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 91; i++)
 	{
 		result = a + b;
-		printf("%lu", result);
-
-		if (i != 97)
-		{
-			printf(", ");
-		}
+		printf("%lu, ", result);
 
 		a = b;
 		b = result;
+	}
+
+	a1 = a / l; 
+	a2 = a % l;
+	b1 = b / l;
+	b2 = b % l;
+
+	for (i = 92; i < 99; i++)
+	{
+		result1 = a1 + b1;
+		result2 = a2 + b2;
+
+		printf("%lu", result1);
+		printf("%lu", result2);
+
+		if (i != 98)
+			printf(", ");
+
+		a1 = b1 + (b2 / l);
+		a2 = b2 % l;
+		b1 = result1 + (result2 / l);
+		b2 = result2 % l;
 	}
 	printf("\n");
 }
