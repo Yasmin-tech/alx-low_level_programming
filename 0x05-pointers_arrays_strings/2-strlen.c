a1da75cfd8a07fcd75422aa91610addae08e03bc
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-06-26 11:47:46 +0000

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that swaps the values of two integers
+ * @s: string pointer
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
