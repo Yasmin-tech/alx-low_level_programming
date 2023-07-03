094a08ac76c49fde6940af1df6c0611623995392
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-03 18:55:41 +0000

    add task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - find the first occurrence of the character
+ *@s: a string pointer
+ *@c: a charcter to look for
+ *
+ * Return: the pointer to the first occurrence
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+
+		s++;
+	}
+
+	return (s);
+}
