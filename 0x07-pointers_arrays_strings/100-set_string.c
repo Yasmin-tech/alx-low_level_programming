5857a2e70bebc8d206470981fa518f556d6d8ba3
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-04 20:11:57 +0000

    add task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - set a string to another string
+ *@s: a pointer to a string to set
+ *@to: a ppointer to a string which we set to
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
