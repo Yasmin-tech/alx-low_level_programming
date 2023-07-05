77738845642694347ecc5a272e113010b594350f
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-05 21:04:05 +0000

    add task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev_recursion - print a string in reverse using recursion
+ *@s: a pointer to a charcter
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int len = strlen(s);
+	int curr_len;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(++s);
+		s--;
+		curr_len = strlen(s);
+		if (curr_len <= len)
+			_putchar(*s);
+	}
+}
