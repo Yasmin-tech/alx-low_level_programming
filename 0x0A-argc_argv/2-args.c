ebca8f7944cb4f2ce800bcdf6f6b59bccb07bccd
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-07 23:02:37 +0000

    add task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ *@argc: a counter of command line arguments
+ *@argv: an array of string pointer
+ *
+ * Return: (0) Successfuly.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
