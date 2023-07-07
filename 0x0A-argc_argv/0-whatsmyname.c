cc5c4d947ea436a673de26fd0006357c5d5ea2c5
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-07 22:45:11 +0000

    edit 1 add README.md and task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * print_program_name - a function that prints the name of
+ * main - a function that prints the name of
  *	the program that its used in
  *@argc: a counter of command line arguments
- *@argv: an array of string pointer 
+ *@argv: an array of string pointer
  *
  * Return: (0) Successfuly.
  */
 
-int main( __attribute__((__unused__)) int argc, char *argv[])
+int main(__attribute__((__unused__)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
