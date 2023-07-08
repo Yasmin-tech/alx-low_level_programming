afc54942f8ffb4cb92571a44666b2c2aa8097365
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-08 01:21:33 +0000

    edit main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef "MAIN.H"
-#define "MAIN.H"
-
+#ifndef MAIN.H
+#define MAIN.H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN.H*
+#endif
