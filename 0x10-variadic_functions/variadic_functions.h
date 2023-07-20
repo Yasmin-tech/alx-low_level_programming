e95c598eb53ca32e4bf61276dfbbbf312f81c7fc
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-20 21:15:44 +0000

    add README.md and variadic_functions.h header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+#endif /* VARIADIC_FUNCTIONS_H */
+
