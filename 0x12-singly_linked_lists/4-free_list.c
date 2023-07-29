1bbaf0dd9192e1117d7694901a0b3f4d47f3eac8
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-29 01:08:59 +0000

    add task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_list a function that frees a list_t list
+ *@head: a pointer to the first node
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+	list_t *prev;
+
+	if (head == NULL)
+		return;
+
+	while (ptr != NULL)
+	{
+		prev = ptr;
+		ptr = ptr->next;
+
+		free(prev->str);
+		free(prev);
+		prev = NULL;
+	}
+
+
+
+}
