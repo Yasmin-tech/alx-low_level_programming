f7ce918db6e0f3a363832ad98542da25a9612f34
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-07-31 08:44:29 +0000

    add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory allocated dynamiclly for singly-linked list
+ *	and set head to NULL
+ *
+ *@head: a pointer to the head pointer of the first element in the list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *prev;
+
+	ptr = *head;
+
+	if (ptr == NULL)
+		return;
+
+	while (ptr != NULL)
+	{
+		prev = ptr;
+		ptr = ptr->next;
+		free(prev);
+	}
+
+	*head = NULL;
+}
