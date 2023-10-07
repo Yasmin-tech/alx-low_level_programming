d2322d11c73543351011dcd7776c66e375653785
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-07 19:40:39 +0300

    edit 3 add that a function that creates the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,11 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	(void)size;
 	if (table == NULL)
 		return (NULL);
 
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		table->array[i] = NULL;
-	}
 	return (table);
 }
